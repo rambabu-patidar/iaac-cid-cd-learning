659f1ef5af06b6fb5ed8a57598e50e2f69775dff
Rambabu Patidar <patidarrambabu135@gmail.com>
2026-06-12 04:22:56 +0530

    adding /api/ram route just for fun

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-app.use(express.json());
-
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/health", (req, res, next) => {
 	res.status(200).json({ status: "healthy", timestamp: new Date() });
@@ -14,6 +16,10 @@ app.get("/api/data", (req, res) => {
 		message: "Hello from Azure Containers!",
 		documentation: "Authentication is skipped. This API is fully public.",
 	});
+});
+
+app.get("/api/ram", (req, res, next) => {
+	res.sendFile(`${path.join(__dirname, "index.html")}`);
 });
 
 app.listen(PORT, () => {
